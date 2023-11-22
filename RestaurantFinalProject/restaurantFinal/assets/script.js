9f063e55e4cc6fd6eb2a16d0c4dc7e8ba1b0a80b
jamilasamadova <126696279+jamilasamadova@users.noreply.github.com>
2023-11-22 22:18:52 +0400

    Add files via upload

---
+++ RestaurantFinalProject/restaurantFinal/assets/script.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper(".sliders", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
